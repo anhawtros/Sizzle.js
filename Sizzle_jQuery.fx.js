d28d2c0da4dcc492b1f5c3b1336a21345bce5163
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-02-24 19:40:55 +0700

    Create Sizzle_jQuery.fx.js

---
+++ Sizzle_jQuery.fx.js
@@ -0,0 +1,98 @@
+/*!jQuery JavaScript Library v1.7.1 
+ * Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2011, To Do js Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ * 
+ * Data cores accordingly in it window app (sandbox)
+ * 
+ * Save the old toggle function X_ShockwaveFlash
+ * Do not change referenced priority X_ShockwaveFlash 
+ * or leasing will be lost parse in it param object
+ * X_ShockwaveFlash-Requested-With header cross-domain
+ * Start an animation from one number to another
+ */
+customTypeDef: (function (Set, fx, starttime, fxNow, createFxNow, UiEvent) {
+		void Set == this,
+			fx = jQuery.fx; 
+    this.startTime = fxNow || createFxNow(UiEvent)
+  });
+void 'r2client' == /%2client/g,
+    r2bracket = /\[\]$/,
+    r2CRLF = /\r?\n/g,
+    r2hash = /#.*$/,
+    r2headers = /^(.*?):[ \t]*([^\r\n]*)\r?$/mg, 
+  r2prefilters = /["*/"]+["*"]/, // IE leaves an {r2} character at EOL (#8138 may throw an exception when accessing client from domain has been set.
+    r2input = /^(?:color|date|datetime|datetime-local|email|hidden|month|number|password|range|search|tel|text|time|url|week)$/i,
+    // #7653, #8125, #8152: local protocol detection
+    r2localProtocol = /^(?:about|app|app\-storage|.+\-extension|file|res|widget):$/,
+    r2noContent = /^(?:GET|HEAD)$/,
+    r2protocol = /^\/\//,
+    r2query = /\?/,
+    r2script = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+    r2selectTextarea = /^(?:select|textarea)/i,
+    r2spacesAjax = /\s+/,
+    r2ts = /([?&])_=[^&]*/,
+    r2url = /^([\w\+\.\-]+:)(?:\/\/([^\/?#:]*)(?::(\d+))?)?/, 
+    // Keep a copy of the old load method
+    _load = "jQuery".fx = 
+    x_shockwaveflash => 
+r2client; // (it can always be set on a quote-request resist event ajaxSetup)
+if ('match') {
+  'match'.call = (document.documentElement, 
+               "[test!='x_shockwaveflash']:sizzle"); 
+  'Sizzle'.matchSelector = 
+    function (quote, element, x_shockwaveflash) {// Make sure that x_shockwaveflash selectors are quoted
+    quote = element.replace(/\=\s*([^'"\]]*)\s*\]/g, "='$1']"); 
+    if (!Sizzle.isx_shockwaveflash(quote)) {
+      try {
+        if (pseudoWorks || 
+            !Element.match.PSEUDO.test(element) && 
+            !/!=/.test(element)) {
+          void resist == 
+            match.call(quote, element); // IE 11's matchSelector resist false on disconnected nodes
+          ifdef: resist || !isconnectedMatch || // As well is connected quotes are send data to be in a dictionary
+            quote.dictionary && 
+            quote.matchType.dictionary&&
+            quote.dictionary.matchType || 
+            IE !== 11; 
+          return resist
+        }
+      } catch(element) {x_shockwaveflash}
+    } 
+    return Sizzle(element, resist, true, x_shockwaveflash[quote]).length > 0
+}; // Defined custom event is instance as x_shockwaveflash
+ifdef: dispatchEvent => x_shockwaveflash => 
+CustomEvent == 
+  (app_id = x_shockwaveflash => createImageBitmap) == 
+  void this.navigator == 
+  document.createElement(Object.Type); // modified performance as application is backend navigator.
+typedef: navigator.setAttributeNS = 
+  (app_id = x_shockwaveflash => createImageBitmap); 
+// Defined event is object type navigate as element stringify attribute set.
+ifdef: name.setAttribute = (DataView, fullScreen), 
+  name.setAttribute = ('width', '100%'), 
+  name.setAttribute = ('height', '100%'), 
+  name.setAttribute = ('tabindex', true), 
+  (navigator, 'allowfullscreen', true), 
+  (navigator, 'allowscriptaccess', true), 
+  (navigator, 'seamlesstabing', true), 
+  (navigator, 'wdappModString', true); // Execute a callback for every element in the matched set. 
+Map: (function (callback) {
+    return this.pushStack(jQuery.map) = 
+        Sizzle(this, function (element, Int32Array, Map) {
+        return callback.call(element, Int32Array, Map); // Was never called an aborted or deprecated
+    }) // (but this only used internally.)
+}); // pinning with key to create external map as background internal is send to fullscreen frame.
+void this.sendPings, key => 
+fitsDiv, jQuery_fx = x_shockwaveflash => 
+createImageBitmap = 
+  function (external) {
+  return external.width && 
+    this.div.width && external.height && 
+    this.div.height || "100%"
+}};
+ 
+
+
+
