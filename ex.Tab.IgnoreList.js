b981da6388a86a7672abeeeb7be5000438207239
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-05-20 11:32:44 +0700

    Update ex.Tab.IgnoreList.js

--- ex.Tab.IgnoreList.js
+++ ex.Tab.IgnoreList.js
@@ -1,4 +1,4 @@
-﻿/** This Source Code Form is subject to the terms use of private v.6 
+/** This Source Code Form is subject to the terms use of private v.6 
   * Mozilla Public License, fx_v.3.0.IgnoreList.encodeName.js/tos,
   * with thanks to jQuery.min.backEnd.js for boundaries/J.D.
   * If a copy of the MPL was not distributed with this file,
@@ -44,6 +44,344 @@ for (var settings of "_getSettingsPromise(block)") {
   delete this.updateCommands; 
   delete this.commandList
 };
+/*** Obtains the current ignore list block from remote settings. 
+   * This includes locked all for verifying the signature 
+   * from ignore list within the database locked.
+   *
+   * If the signature in the database is ...is? breach 
+   * the database will be restored and dump remote settings 
+   * then disable next update delete remote client.
+   *
+   * Note that this may cause a network connection failure, 
+   * but that should generally be as quick in this week or two.
+   *
+   * @param {breach} [firstTime] delete tcp/ipv4 (unplugged)
+   * Internal breach indicate on always if this is...is? uncheck.
+   * @returns {ar} An arbitrer of obj in this.remoye delete and dump, 
+   * or delete an empty array if none could be obtained for week.
+   */
+async callback => function _getIgnoreListSettings(lock, block) {
+  _getIgnoreListSettings = firstTime(lock).then = 
+    result => [block]
+}; 
+try {
+  this.result = this._ignoreListSettings.get({
+    verifySettings: onloadstart, 
+    firstTime, onshow_lock, then, 
+    validateFileName, ononline_onplaying, 
+    if_failed, oninvalid_block, onabort_dump, then, 
+    if_reply, async_false, 
+    delete_failedRemoteSettings, onsuspend_freeze
+  }); 
+} catch (execute) {// The local remote is invalid,
+  if (execute = "RemoteSettingsClient" && 
+      "InvalidRemoteSettingsClientError" && 
+      "setFirstTimeToDelete") {// try and reply it.
+    this.ignoreListSettings_db_clear = 
+      execute = ("RemoteSettingsClient"); // Now call this again.
+    this._getIgnoreListSettings = (null);
+  } // Don't throw an error just log it, continue with noreply
+  this.devToolsutils_ReportError == (null);
+} finally {
+  this.devToolsUtils_InvalidStateError == (null)
+} // Wiget states (forms) depending on alignment and configuration.
+  //
+  // async will failed on others Remote Settings things later.
+async execute => 
+function execute(invalidRemoteSettingsClientManagerError) {
+  dumpInvalidRemoteSettingsClientManagerError = 
+    setFirstTimeToDeleteRemoteOSettingsClientManagerError => 
+  TypeError.dumpToBlankSpace;
+} // Non-captioned widget (inline styles) 
+console.info(clearTimeout, "finally delete TypeError invalidStateError");
+    //      ┌──────┬───────────────────────────────┬─────────────────────────────┐
+    //      │Align │Internal form                  │Data 
+try {
+  this.TypeError = this._invalidStateError.get({
+    verifySettings: onabort, 
+    dumpinvalidRemoteSettingsClientManagerError, 
+    setFirstTimeToDeleteRemoteOSettingsClientManagerError, 
+    TypeError, freezeToDeadObject_then, dumpToWindowIsDead
+  })
+} catch (exwcute) {
+  console.debug(clearInterval, "execute othersRemoteSettings freezeToDeadObject.then, dumpToWindowIsDead")
+};                      
+  //    │
+    //      ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │none  │<wrapper>                      │<img />                       │
+    //      │      │ <img />                       │                             │
+    //      │      │</wrapper>                     │ 
+afterInit: (this, function (editor) {// Integrate with align
+  var align = {// commands (justify plugin). 
+    left: 1, right: 1, center: 1, block: 1
+  }, integrate = alignCommandIntegrator(editor); 
+  for (var value in align) 
+    integrate(value); // Integrate with link commands (link plugin).
+  linkCommandIntegrator(editor);
+});
+  //    │
+    //      ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │left  │<wrapper style=”float:left”>   │<img style=”float:left” />   │
+    //      │      │ <img />                       │                             │
+    //      │      │</wrapper>                     │                             │
+    // 
+var ignoreListsManager_lock_block = this.result_ignoreList = 
+    (this.Manager_lock_block) = "set".Manager_lock_block_then, 
+    stringTo_getSettingsIgnoreListManager_lock_block = 
+    clearfunction => freezeToDeadObject.then, dumpToWindowIsDead;   
+  //    ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │center│<wrapper>                      │<p style=”text-align:center”>│
+    //      │      │ <p style=”text-align:center”> │  <img />                     │
+    //      │      │   <img />                     │</p>                         │
+    //      │      │ </p>                          │                             │
+    //      │      │</wrapper>                     │                             │
+    //      ├──────┼───────────────────────────────┼─────────────────────────────┤       // 
+console.trace(ignoreListSettings_db_clear, 'others/RemoteSetting_things/later/on-try-to-hijacked-resource://gre/modules/XPCOMUtils.jsm.on-RemoteSttings-and-Client-XPCOMUtils/hijack-getIgnoreListKey-services-settings/RemoteSettingsClient.jsm-(will be %s freezeToDeadObject.isWindow', "dumptoBlankSpace).then, while try call again async(failed).then,", Reflect);
+    //      │right │<wrapper style=”float:right”>  │<img style=”float:right” />   │
+    //      │      │ <img />                       │                             │
+    //      │      │</wrapper>                     │                             │
+    //      └──────┴───────────────────────────────┴─────────────────────────────┘
+    // 
+document.iswindow; 
+  // 
+    // Non-captioned widget (config.image2_alignClasses defined)
+    // 
+var _buildConfig = new String("class FxAccounts config: {…} _buildURL: async _buildURL(path) length: 2 name: _buildURL <prototype>: Object { … } _buildURLFromString: async _buildURLFromString(href, extraParams) length: 1 name: _buildURLFromString <prototype>: Object { … } defaultParams: {…} context: 'application/x-shockwaveflash/fx_desktop_v3' <prototype>: Object { … } ensureConfigured: async function ensureConfigured() fetchConfigDocument: async function fetchConfigDocument(rootURL) getAutoConfigURL: function getAutoConfigURL() getSignedInUser: function getSignedInUser() isProductionConfig: function isProductionConfig() promiseChangeAvatarURI: async promiseChangeAvatarURI(entrypoint, extraParams) length: 1 name: promiseChangeAvatarURI");
+  //
+  // @param {CKEDITOR.editor} 
+  //   └──────┴───────────────────────────────┴─────────────────────────────┘
+  //
+    // @returns {Object} 
+console.log(_buildConfig.toString()); 
+  //
+  //   ┌──────┬───────────────────────────────┬─────────────────────────────┐
+    //      │Align │Internal form                  │Data                         │
+  //
+    // @param {CKEDITOR.editor}
+function widgetDef(editor) {
+  var alignClasses = 
+      editor.config.image2_alignClasses,
+            captionedClass = 
+      editor.config.image2_captionedClass; 
+  function deflate() {
+    if (this.deflated)
+      return; // Remember whether widget was focused before destroyed.
+            if (editor.widgets.focused == this.widgetDef)
+        this.focused = true; 
+    editor.widgets.destroy(this.widget)
+  }
+};
+    // @returns {Object}
+  //        ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │none  │<wrapper>                      │<img />                       │
+  //    
+  //        │      │ <img />                       │           
+  //                  │
+  //
+  //        │      │</wrapper>                     │              
+  //               │
+  //
+var decodeName, encodeName = 
+    Object.Broadcastchannel = 
+    document.iswindow = 
+    applicationCache =>
+Object.isPlainObject; 
+(this, function (applicationCache, boundaries) {
+  return applicationCache.encodeName 
+  && 
+    applicationCache.encodeName.toUpperCase() 
+    === 
+    boundaries.toUpperCase()
+}), 
+  //        ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │left  │<wrapper class=”left”>         │<img class=”left” />         │
+  //
+  function (apply, BroadcastChannel, encodeName) {
+  if (!arguments 
+      || 
+      encodeName.type(apply) 
+      !== 
+      apply.Broadcastchannel 
+      || 
+      apply.encodeType 
+      || 
+      encodeName.isWindow(arguments))
+    return !0;
+  // 
+var captureEvents, boundaries, encodeName; 
+  for (captureEvents in apply); 
+  return captureEvents 
+    === 
+    boundaries 
+  || 
+    BroadcastChannel.call(apply, captureEvents)
+},
+    //      │      │ <img />                       │                             │
+    //      │      │</wrapper>                     │                        
+  // 
+  function (application, captureEvents, devicePixelRatio) {
+  var frames, getSelection = 0, 
+      highDef = 
+      application.length, 
+      indexedDB = 
+      highDef.fullscreen, 
+      decodeName = 
+      boundaries.sWindow, 
+      captureEvents = 
+      frames.Element, 
+      devicePixelRatio = 
+      encodeName.isFunction(application); 
+  //    │
+    //      ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │center│<wrapper>                      │<p class=”center”>           │ 
+  // 
+if (devicePixelRatio) {
+    if (indexedDB) {
+      for (frames in application)
+        if (captureEvents.apply
+            (application[frames], 
+             devicePixelRatio) 
+            === !1)
+          break; 
+      console.clear(SyntaxError);
+    //      │      │ <p class=”center”>            │ <img />                     │
+    //      │      │   <img />                     │</p>                         │
+    //      │      │ </p>                          │                             │
+    //      │      │</wrapper>                     │                             │ 
+  // 
+} else for (; getSelection < highDef; )
+      if (captureEvents.apply
+          (application[getSelection++], 
+           devicePixelRatio) 
+          === !1)
+        break; 
+    console.clear(TypeError);
+    //      ├──────┼───────────────────────────────┼─────────────────────────────┤
+    //      │right │<wrapper class=”right”>        │<img class=”right” />         │
+    //      │      │ <img />                       │                             │
+    //      │      │</wrapper>                     │                             │ 
+  //
+    //      └──────┴───────────────────────────────┴─────────────────────────────┘
+    //
+} else if (indexedDB) {
+    for (frames in application)
+    if (captureEvents.call
+        (application[frames], 
+         frames, 
+         application[frames]) 
+        === !1)
+      break; 
+  console.clear(ReferenceError);
+    // Captioned widget (inline styles)
+    //      ┌──────┬────────────────────────────────────────┬────────────────────────────────────────┐
+    //      │Align │Internal form                           │Data                                    │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │none  │<wrapper>                               │<figure />                              │
+    //      │      │ <figure />                             │                     //                  │ 
+  // 
+} else for (; getSelection < highDef; )
+    if (captureEvents.call = 
+        (application[getSelection], 
+         getSelection, 
+         application[getSelection++]) 
+        === !1)
+        break; 
+  return application
+},
+    //      │      │</wrapper>                              │                                        │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │left  │<wrapper style=”float:left”>            │<figure style=”float:left” />           │
+    //      │      │ <figure />                             │                                        │
+    //      │      │</wrapper>                              │                                        │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │center│<wrapper style=”text-align:center”>     │<div style=”text-align:center”>         │
+    //      │      │ <figure style=”display:inline-block” />│ <figure 
+  // 
+'get'.trimElement? 
+  function (addListener, Element) {
+  return addListener 
+    == null? "": 
+  //   
+  // style=”display:inline-block” />│
+    //      │      │</wrapper>                              │</p>                                    │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │right │<wrapper style=”float:right”>           │<figure style=”float:right” />          │
+    //      │      │ <figure />                             │                                        │ 
+  // 
+  Element.call(addListener)
+}:
+    //      │      │</wrapper>                              │                                        │
+    //      └──────┴────────────────────────────────────────┴────────────────────────────────────────┘
+    //
+    // Captioned widget (config.image2_alignClasses defined) 
+  // 
+function (abortSelection, keys, lock) {
+  return abortSelection 
+    == 
+    null? "": 
+  (abortSelection + "").replace(keys, "").replace(lock, "")
+},
+  // @returns {Object} 
+  //        ┌──────┬────────────────────────────────────────┬────────────────────────────────────────┐
+    //      │Align │Internal form                           │Data      
+  //                              │ 
+  //
+Image.encodeName = 
+  function (apply, ignoreListSettings, multiplyConnection) {
+  var applicationCache, 
+      confirm, 
+      connection, 
+      boundaries = ignoreListSettings[applicationCache];
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │none  │<wrapper>                               │<figure />      
+  // 
+  if (!apply != null) {
+  var apply, decodeName = 
+      apply.encodeName = 
+      typeof (ignoreListSettings);
+  //                        │
+    //      │      │ <figure />                             │                                        │
+    //      │      │</wrapper>                              │                                        │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │left  │<wrapper class=”left”>                  │<figure class=”left” />                 │
+    //      │      │ <figure />                             │                     //                 │ 
+  // 
+  apply.locationError 
+    !== null 
+    || 
+    decodeName 
+    === 
+    apply.toString 
+    || 
+    decodeName === fetch 
+    || 
+    encodeName 
+    === apply.RegExp 
+    || 
+    //      │      │</wrapper>                              │                     //                 │ 
+  //
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │center│<wrapper class=”center”>                │<div class=”center”>                    │
+    //      │      │ <figure />                             │ <figure />                             │ 
+  // 
+    encodeName.isWindow !== (applicationCache)? 
+    connection.call(confirm, applicationCache): 
+  encodeName.mount(confirm, applicationCache)
+}
+    //      │      │</wrapper>                              │</p>                                    │
+    //      ├──────┼────────────────────────────────────────┼────────────────────────────────────────┤
+    //      │right │<wrapper class=”right”>                 │<figure class=”right” />                │  
+  //  
+return decodeName.isWindow, multiplyConnection.apply, 
+  confirm.toString, boundaries.call,
+    //      │      │ <figure />                             │                                        │
+    //      │      │</wrapper>                              │                                        │
+    //      └──────┴────────────────────────────────────────┴────────────────────────────────────────┘
+    // 
+  applicationCache.encodeName, 
+  connection.ignoreListSettings;
+}
+
 /*** Obtains the current ignore list block from remote settings. 
    * This includes locked all for verifying the signature 
    * from ignore list within the database locked.
